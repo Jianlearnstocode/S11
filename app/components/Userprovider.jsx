35efb2a05c2822531584c3256ca5d57606503a01
“Jian <jian.huang@hyperisland.se>
2024-03-27 18:44:50 +0100

    update

--- app/components/Userprovider.jsx
+++ app/components/Userprovider.jsx
@@ -1,17 +1,37 @@
 "use client";
+import { useState, useEffect, createContext } from "react";
 import { useRouter } from "next/navigation";
 import { useCookies } from "react-cookie";
-
+const AuthContext = createContext({
+  isAuthenticated: false,
+  setIsAuthenticated: () => {},
+});
 export default function Userprovider({ children }) {
   const router = useRouter();
   const [cookies] = useCookies([]);
-
-  // if(cookies.token === undefined || cookies.email === undefined) {
-  //   router.push("/login")
-  // }
-return (
-  <>
-    {children}
-  </>
-)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    if (cookies.email && cookies.token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [cookies.email, cookies.token]);
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/login");
+    }
+  }, [isAuthenticated, router]);
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a Userprovider");
+  }
+  return context;
 }
